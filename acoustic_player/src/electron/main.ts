067668b24a9201584b906d0cccea178e7372e110
Zihao <zihaofu12@gmail.com>
2025-06-28 13:39:35 -0700

    feat: initialize acoustic player application with Electron and React
    - Added package.json for project dependencies and scripts.
    - Created main Electron process in src/electron/main.ts.
    - Configured TypeScript for Electron in src/electron/tsconfig.json.
    - Implemented development utility function in src/electron/utils.ts.
    - Developed basic UI components in src/ui/App.tsx with styling in src/ui/App.css.
    - Included React logo asset in src/ui/assets/react.svg.
    - Set up global styles in src/ui/index.css.
    - Configured main entry point for React application in src/ui/main.tsx.
    - Defined TypeScript environment for Vite in src/ui/vite-env.d.ts.
    - Established TypeScript configuration for application and node in tsconfig.app.json and tsconfig.node.json.
    - Configured Vite for building and serving the application in vite.config.ts.

---
+++ acoustic_player/src/electron/main.ts
@@ -0,0 +1,19 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+import path from 'path';
+import { isDev } from './utils.js';
+
+type test = String;
+
+app.on('ready', () => {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    if (isDev()) {
+        mainWindow.loadURL("http://localhost:5123")
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'));
+    }
+
+})
